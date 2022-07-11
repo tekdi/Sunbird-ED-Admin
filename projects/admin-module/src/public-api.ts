3e7ab607afe6a5563810910d0f0bd1617e189449
Kshitija Kadam <kshitijakadam04@gmail.com>
2022-07-11 14:23:46 +0530

    chore: Added initial library structure

---
+++ projects/admin-module/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of admin-module
+ */
+
+export * from './lib/admin-module.service';
+export * from './lib/admin-module.component';
+export * from './lib/admin-module.module';
